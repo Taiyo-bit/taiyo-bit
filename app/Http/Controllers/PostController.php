7cbdee8094cbbe061a03def67dbc5bf2083bc3c4
Taiyo-bit <taiyou/tkj@icloud.com>
2022-11-16 11:22:45 +0900

    08-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
-use App\Models\Post; 
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-    return $post->get();
-    }
+   public function index(Post $post)
+   {
+    return view('posts/index')->with(['posts' => $post->getByLimit()]);
+       
+   }
 }
